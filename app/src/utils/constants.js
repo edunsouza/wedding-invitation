51dabfeb07a9687b50397eb3f23dfcf296218696
edunsouza <eduardo.souza.ens@gmail.com>
2022-01-17 02:17:34 -0300

    new constants/variables

--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -1,7 +1,9 @@
 export const EVENT_OWNERS = 'Eduardo & Camila';
 export const EVENT_DATE_STRING = '16 de Abril de 2022';
-export const EVENT_DATE = '2022-04-16T17:00:00';
-export const EVENT_ADDRESS = 'Estrada José Zingano, 1000 - Distrito Industrial, Cachoeirinha - RS, 94930-280';
+export const EVENT_START = '17:00';
+export const EVENT_DATE = '2022-04-16T20:00:00Z';
+export const EVENT_ADDRESS = 'Estrada José Zingano, 1000 - Cachoeirinha - RS';
 export const EVENT_CONTACT = '(51) 99711-5522';
+export const EVENT_GIFTS_LIST = 'https://www.querodecasamento.com.br/lista-de-casamento/dudu-camila';
 export const EVENT_PLACE_WEBSITE = 'http://www.alamedafigueira.com.br/';
 export const EVENT_MAP_EMBED = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3552.5819101876036!2d-51.114620887706046!3d-29.921139324038403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x818ee13e88335be9!2zMjnCsDU1JzE2LjEiUyA1McKwMDYnNDguMSJX!5e1!3m2!1spt-BR!2sbr!4v1641000741148!5m2!1spt-BR!2sbr';
