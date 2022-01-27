9757332640e530dfe9f1a1b71e9ea9ca1375da14
edunsouza <eduardo.souza.ens@gmail.com>
2022-01-27 04:27:07 -0300

    utils changes

--- app/src/utils/string.js
+++ app/src/utils/string.js
@@ -1,3 +1,5 @@
 export const capitalize = text => {
     return text?.toLowerCase()?.replace(/^./, char => char?.toUpperCase());
 };
+
+export const doubleDigit = (number) => String(number).padStart(2, '0');
